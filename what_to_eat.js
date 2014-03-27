e86bf0d51c1fb8e56c1cab27714b7720da054c09
rosy1014 <rosy1014@163.com>
2014-03-27 15:42:17 -0700

    Second Version

---
+++ what_to_eat.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Ruoxi Lu on 3/27/14.
+ */
+
+
+/*
+ addDish()
+ Let user modify the menu, add new items onto the menu.
+ Take user input and set off shuffling at the same time.
+ */
+
+function addDish() {
+    menu = $('#myMenu').val().split("，");
+    var newMenu = [];
+
+    for (i = 0; i < menu.length; i++) {
+        if (menu[i] != '') {
+            newMenu.push(menu[i]);
+        }
+    }
+    menu = newMenu;
+    alert(menu);
+    start();
+}
+
+
+var timer;
+
+/*
+ startShuffle()
+ Set off suffling.
+ */
+function startShuffle() {
+    clearInterval(timer);
+    timer = setInterval('shuffleDisplay()', 80);
+}
+
+/*
+ confirm to stop shuffling and make a choice
+ */
+//
+function stopShuffle() {
+    clearInterval(timer);
+}
+
+/*
+ start to shuffle
+ */
+
+function shuffleDisplay() {
+    $('#shuffleArea').html(menu[ parseInt(Math.random() * menu.length)]);
+}
+
